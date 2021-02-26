e84719732a9ff56c9c4a0c47993276730688fad4
NuBiFoRM <nubiform@me.com>
2021-02-26 20:08:27 +0900

    sql script 작성

--- mysql.sql
+++ mysql.sql
@@ -5,38 +5,26 @@ USE `idus`;
 DROP TABLE IF EXISTS `members`;
 
 CREATE TABLE `members` (
-  `memberNumber` int(11) NOT NULL,
+  `memberId` varchar(50) NOT NULL,
   `memberName` varchar(50) NOT NULL,
-  `contactLastName` varchar(50) NOT NULL,
-  `contactFirstName` varchar(50) NOT NULL,
+  `nickName` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
   `phone` varchar(50) NOT NULL,
-  `addressLine1` varchar(50) NOT NULL,
-  `addressLine2` varchar(50) DEFAULT NULL,
-  `city` varchar(50) NOT NULL,
-  `state` varchar(50) DEFAULT NULL,
-  `postalCode` varchar(15) DEFAULT NULL,
-  `country` varchar(50) NOT NULL,
-  `salesRepEmployeeNumber` int(11) DEFAULT NULL,
-  `creditLimit` decimal(10,2) DEFAULT NULL,
-  PRIMARY KEY (`memberNumber`)
+  `email` varchar(50) NOT NULL,
+  `gender` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`memberId`)
 ) ENGINE=InnoDB;
 
-insert  into `members`(`memberNumber`,`memberName`,`contactLastName`,`contactFirstName`,`phone`,`addressLine1`,`addressLine2`,`city`,`state`,`postalCode`,`country`,`salesRepEmployeeNumber`,`creditLimit`) values 
+insert  into `members`(`memberId`,`memberName`,`nickName`,`password`,`phone`,`email`) values
 
-(103,'Atelier graphique','Schmitt','Carine ','40.32.2555','54, rue Royale',NULL,'Nantes',NULL,'44000','France',1370,'21000.00'),
-
-(112,'Signal Gift Stores','King','Jean','7025551838','8489 Strong St.',NULL,'Las Vegas','NV','83030','USA',1166,'71800.00');
+('nubiform','SangQ','NuBiFoRM','1234','1234','nubiform@abc.com');
 
 DROP TABLE IF EXISTS `orders`;
 
 CREATE TABLE `orders` (
-  `orderNumber` int(11) NOT NULL,
-  `orderDate` date NOT NULL,
-  `requiredDate` date NOT NULL,
-  `shippedDate` date DEFAULT NULL,
-  `status` varchar(15) NOT NULL,
-  `comments` text,
-  `memberNumber` int(11) NOT NULL,
+  `orderNumber` int(12) NOT NULL,
+  `orderDate` timestamp NOT NULL,
+  `memberNumber` int(12) NOT NULL,
   PRIMARY KEY (`orderNumber`),
   KEY `memberNumber` (`memberNumber`),
   CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`memberNumber`) REFERENCES `members` (`memberNumber`)
@@ -47,3 +35,12 @@ insert  into `orders`(`orderNumber`,`orderDate`,`requiredDate`,`shippedDate`,`st
 (10100,'2003-01-06','2003-01-13','2003-01-10','Shipped',NULL,103),
 
 (10101,'2003-01-09','2003-01-18','2003-01-11','Shipped','Check on availability.',112);
+
+CREATE TABLE `orderdetails` (
+  `orderNumber` int(12) NOT NULL,
+  `detailNumber` int(12) NOT NULL,
+  `product` varchar(50) NOT NULL,
+  PRIMARY KEY (`orderNumber`, `orderNumber`),
+  KEY `memberNumber` (`memberNumber`),
+  CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`memberNumber`) REFERENCES `members` (`memberNumber`)
+) ENGINE=InnoDB;
