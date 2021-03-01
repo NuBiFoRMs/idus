6c9cf33b0b7a93d750085e40662e02fa0659d79f
NuBiFoRM <nubiform@me.com>
2021-03-01 15:18:26 +0900

    Email 형식 validation

--- src/main/java/com/nubiform/idus/api/member/service/MemberService.java
+++ src/main/java/com/nubiform/idus/api/member/service/MemberService.java
@@ -29,6 +29,12 @@ public class MemberService {
     }
 
     public boolean signUp(Member member) {
+        // email validation
+        String emailRegex = "^[a-zA-Z0-9_!#$%&’*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$";
+        Pattern emailPattern = Pattern.compile(emailRegex);
+        if (!emailPattern.matcher(member.getEmail()).matches())
+            throw IdusException.of("invalid email address");
+
         if (memberMapper.getMember(member.getMemberId()) != null)
             throw IdusException.of("duplicate user account");
 
