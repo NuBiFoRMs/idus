cd6b119075240c6db276771b16af69359c502a48
NuBiFoRM <nubiform@me.com>
2021-03-02 18:49:19 +0900

    Spring Security 적용

---
+++ src/main/java/com/nubiform/idus/config/security/WebSecurityConfig.java
@@ -0,0 +1,19 @@
+package com.nubiform.idus.config.security;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic()
+                .and()
+                .authorizeRequests()
+                .antMatchers("/api/**/order/**").hasRole("USER")
+                .antMatchers("/api/**/member/**").permitAll()
+                .antMatchers("/swagger-ui/**").permitAll();
+    }
+}
