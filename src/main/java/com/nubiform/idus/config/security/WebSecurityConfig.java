c599df226271643b63ae5919c40f52bc9617fd83
NuBiFoRM <nubiform@me.com>
2021-03-02 21:10:15 +0900

    JwtAuthenticationFilter 생성 및 등록

--- src/main/java/com/nubiform/idus/config/security/WebSecurityConfig.java
+++ src/main/java/com/nubiform/idus/config/security/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -17,6 +18,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers("/api/**/order/**").hasRole("USER")
                 .antMatchers("/api/**/member/**").permitAll()
-                .antMatchers("/swagger-ui/**").permitAll();
+                .antMatchers("/swagger-ui/**").permitAll()
+                .and()
+                .addFilterBefore(new JwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 }
