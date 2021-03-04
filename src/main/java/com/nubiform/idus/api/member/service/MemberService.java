b678ef61c34d58ef111cb244edccf76849ccd7c9
NuBiFoRM <nubiform@me.com>
2021-03-04 23:56:02 +0900

    회원가입 Validation 수정

--- src/main/java/com/nubiform/idus/api/member/service/MemberService.java
+++ src/main/java/com/nubiform/idus/api/member/service/MemberService.java
@@ -35,32 +35,32 @@ public class MemberService {
         // memberId validation
         String memberIdRegex = "^[a-zA-Z]{8,}$";
         if (!member.getMemberId().matches(memberIdRegex))
-            throw IdusException.of("invalid user id");
+            throw IdusException.of("invalid user id (영문 대소문자만 허용, 최소 8자 이상)");
 
         // memberName validation
         String memberNameRegex = "^[a-zA-Z가-힣]+$";
         if (!member.getMemberName().matches(memberNameRegex))
-            throw IdusException.of("invalid user name");
+            throw IdusException.of("invalid user name (한글, 영문 대소문자만 허용)");
 
         // nickName validation
         String nickNameRegex = "^[a-z]+$";
         if (!member.getNickName().matches(nickNameRegex))
-            throw IdusException.of("invalid nickname");
+            throw IdusException.of("invalid nickname (영문 소문자만 허용)");
 
         // password validation
-        String passwordRegex = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[$@$!%*?&])[A-Za-z\\d$@$!%*?&]{8,}";
+        String passwordRegex = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[$@$!%*?&])[A-Za-z\\d$@$!%*?&]{10,}";
         if (!member.getPassword().matches(passwordRegex))
-            throw IdusException.of("invalid password");
+            throw IdusException.of("invalid password (영문 대문자, 영문 소문자, 특수 문자, 숫자 각 1개 이상씩 포함, 최소 10자 이상)");
 
         // phone validation
         String phoneRegex = "^[0-9]+$";
         if (!member.getPhone().matches(phoneRegex))
-            throw IdusException.of("invalid phone number");
+            throw IdusException.of("invalid phone number (숫자)");
 
         // email validation
         String emailRegex = "^[a-zA-Z0-9_!#$%&’*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$";
         if (!member.getEmail().matches(emailRegex))
-            throw IdusException.of("invalid email address");
+            throw IdusException.of("invalid email address (이메일 형식)");
 
         // duplicate validation
         if (memberMapper.getMember(member.getMemberId()) != null)
