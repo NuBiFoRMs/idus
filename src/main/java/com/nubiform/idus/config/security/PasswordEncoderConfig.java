de637728028bb06ba3a3ad006aec05a312339a83
NuBiFoRM <nubiform@me.com>
2021-03-04 23:55:22 +0900

    PasswordEncoder Bean 수정

--- src/main/java/com/nubiform/idus/config/security/PasswordEncoderConfig.java
+++ src/main/java/com/nubiform/idus/config/security/PasswordEncoderConfig.java
@@ -1,9 +1,11 @@
 package com.nubiform.idus.config.security;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+@EnableWebSecurity
 public class PasswordEncoderConfig {
 
     @Bean
