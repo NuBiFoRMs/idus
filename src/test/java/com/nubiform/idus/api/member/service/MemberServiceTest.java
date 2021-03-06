6f25b9bc409dd31bdc3663c60b589a992113b84e
NuBiFoRM <nubiform@me.com>
2021-03-06 13:11:19 +0900

    MemberServicee Test 코드 작성

--- src/test/java/com/nubiform/idus/api/member/service/MemberServiceTest.java
+++ src/test/java/com/nubiform/idus/api/member/service/MemberServiceTest.java
@@ -6,11 +6,8 @@ import com.nubiform.idus.config.error.IdusException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,37 +24,62 @@ class MemberServiceTest {
     @MockBean
     private MemberMapper memberMapper;
 
-    @MockBean
-    private PasswordEncoder passwordEncoder;
-
-    @MockBean
-    private StringRedisTemplate redisTemplate;
-
     @BeforeEach
     void before() {
-        this.memberService = new MemberService(memberMapper, passwordEncoder, redisTemplate);
-
+        this.memberService = new MemberService(memberMapper);
     }
 
     @Test
-    @DisplayName("테스트 명")
+    @DisplayName("단일 회원 상세 정보 조회")
     void getMember() {
+        Member someMember = new Member();
+        someMember.setMemberId("user");
 
-        when(memberMapper.getMembers()).thenReturn(new ArrayList<>());
+        when(memberMapper.getMember("user")).thenReturn(someMember);
+
+        Member member = memberService.getMember("user");
+
+        assertEquals("user", member.getMemberId());
+
+        verify(memberMapper).getMember("user");
+    }
+
+    @Test
+    @DisplayName("단일 회원 상세 정보 조회 예외상황")
+    void getMemberException() {
+        when(memberMapper.getMember("user")).thenReturn(null);
+
+        IdusException exception = assertThrows(IdusException.class, () -> memberService.getMember("user"));
+
+        assertEquals(500, exception.getStatus());
+        verify(memberMapper, times(1)).getMember("user");
+    }
+
+    @Test
+    @DisplayName("전체 회원 정보 조회")
+    void getMembers() {
+        List<Member> memberList = new ArrayList<>();
+        Member member = new Member();
+        member.setMemberId("user01");
+        memberList.add(member);
+        member = new Member();
+        member.setMemberId("user02");
+        memberList.add(member);
+
+        when(memberMapper.getMembers()).thenReturn(memberList);
 
         List<Member> members = memberService.getMembers();
 
-        assertEquals(0, members.size());
+        assertEquals(2, members.size());
     }
 
     @Test
-    @DisplayName("exception check")
-    void exception() {
+    @DisplayName("전체 회원 정보 조회 예외상황")
+    void getMembersException() {
+        when(memberMapper.getMembers()).thenReturn(new ArrayList<>());
 
         IdusException exception = assertThrows(IdusException.class, () -> memberService.getMembers());
-        assertEquals(500, exception.getStatus());
 
-//        verify(memberMapper, times(1)).getMembers();
-        verify(memberMapper, never()).getMembers();
+        verify(memberMapper, times(1)).getMembers();
     }
 }
