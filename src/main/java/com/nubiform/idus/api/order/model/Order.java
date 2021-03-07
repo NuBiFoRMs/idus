3f3778193b47115929beb7edac9c09e6236c7cdc
NuBiFoRM <nubiform@me.com>
2021-03-07 23:10:07 +0900

    localdatetime jsonformat 설정

--- src/main/java/com/nubiform/idus/api/order/model/Order.java
+++ src/main/java/com/nubiform/idus/api/order/model/Order.java
@@ -1,5 +1,6 @@
 package com.nubiform.idus.api.order.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.time.LocalDateTime;
@@ -8,6 +9,7 @@ import java.time.LocalDateTime;
 public class Order {
     private String orderNumber;
     private String productName;
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime paymentDate;
     private String memberId;
 }
