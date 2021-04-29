0bf46241a529a0fb5a94d898002bc8a6a06b76ca
NuBiFoRM <nubiform@me.com>
2021-04-29 19:56:55 +0900

    테스트 코드 정리

--- src/test/java/com/nubiform/idus/api/auth/controller/AuthControllerTest.java
+++ src/test/java/com/nubiform/idus/api/auth/controller/AuthControllerTest.java
@@ -1,4 +1,4 @@
-package com.nubiform.idus.api.member.controller;
+package com.nubiform.idus.api.auth.controller;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,27 +8,34 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 @ActiveProfiles("test")
 @SpringBootTest
 @AutoConfigureMockMvc
-class MemberControllerTest {
+class AuthControllerTest {
 
     @Autowired
     MockMvc mockMvc;
 
     @Test
-    public void member() throws Exception {
-        mockMvc.perform(get("/api/v1/member/member")
+    public void signIn() throws Exception {
+        mockMvc.perform(post("/api/v1/auth/sign-in")
                 .contentType(MediaType.APPLICATION_JSON))
                 .andDo(print());
     }
 
     @Test
-    public void members() throws Exception {
-        mockMvc.perform(get("/api/v1/member/members")
+    public void signUp() throws Exception {
+        mockMvc.perform(post("/api/v1/auth/sign-up")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print());
+    }
+
+    @Test
+    public void signOut() throws Exception {
+        mockMvc.perform(post("/api/v1/auth/sign-out")
                 .contentType(MediaType.APPLICATION_JSON))
                 .andDo(print());
     }
--- src/test/java/com/nubiform/idus/api/order/controller/OrderControllerTest.java
+++ src/test/java/com/nubiform/idus/api/order/controller/OrderControllerTest.java
@@ -1,127 +1,35 @@
 package com.nubiform.idus.api.order.controller;
 
-import com.nubiform.idus.AbstractControllerTest;
-import com.nubiform.idus.api.order.model.MemberOrder;
-import com.nubiform.idus.api.order.model.Order;
-import com.nubiform.idus.api.order.repository.OrderMapper;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
 
-import java.util.ArrayList;
-import java.util.List;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
-import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-class OrderControllerTest extends AbstractControllerTest {
+@ActiveProfiles("test")
+@SpringBootTest
+@AutoConfigureMockMvc
+class OrderControllerTest {
 
     @Autowired
-    private OrderController orderController;
-
-    @Override
-    protected Object controller() {
-        return orderController;
-    }
-
-    @MockBean
-    private OrderMapper orderMapper;
+    MockMvc mockMvc;
 
     @Test
-    @DisplayName("URI: /api/v1/order/orders")
-    void getOrders() throws Exception {
-        List<Order> orderList = new ArrayList<>();
-        Order order = new Order();
-        order.setMemberId("user");
-        orderList.add(order);
-        when(orderMapper.getOrders("user")).thenReturn(orderList);
-
-        String token = jwtTokenProvider.createToken("user");
-
-        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
-        params.add("id", "user");
-        mockMvc.perform(get("/api/v1/order/orders")
-                .header("Authorization", "Bearer " + token)
-                .params(params))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.[0].memberId").value("user"));
-
-        verify(orderMapper, times(1)).getOrders("user");
+    public void orders() throws Exception {
+        mockMvc.perform(post("/api/v1/order/orders")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print());
     }
 
     @Test
-    @DisplayName("URI: /api/v1/order/orders Exception")
-    void getOrdersException() throws Exception {
-        when(orderMapper.getOrders("user")).thenReturn(new ArrayList<>());
-
-        String token = jwtTokenProvider.createToken("user");
-
-        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
-        params.add("id", "user");
-        mockMvc.perform(get("/api/v1/order/orders")
-                .header("Authorization", "Bearer " + token)
-                .params(params))
-                .andExpect(status().isInternalServerError());
-
-        verify(orderMapper, times(1)).getOrders("user");
-    }
-
-    @Test
-    @DisplayName("URI: /api/v1/order/orders : Unauthorized")
-    void getOrdersUnauthorized() throws Exception {
-        mockMvc.perform(get("/api/v1/order/orders"))
-                .andExpect(status().isUnauthorized());
-    }
-
-    @Test
-    @DisplayName("URI: /api/v1/order/member-orders")
-    void getMemberOrders() throws Exception {
-        List<MemberOrder> memberOrderList = new ArrayList<>();
-        MemberOrder memberOrder = new MemberOrder();
-        memberOrder.setMemberName("username");
-        memberOrderList.add(memberOrder);
-        when(orderMapper.getMemberOrders("username", null, PageRequest.of(1, 10))).thenReturn(memberOrderList);
-
-        String token = jwtTokenProvider.createToken("user");
-
-        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
-        params.add("name", "username");
-        mockMvc.perform(get("/api/v1/order/member-orders")
-                .header("Authorization", "Bearer " + token)
-                .params(params))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.[0].memberName").value("username"));
-
-        verify(orderMapper, times(1)).getMemberOrders("username", null, PageRequest.of(1, 10));
-    }
-
-    @Test
-    @DisplayName("URI: /api/v1/order/member-orders : Exception")
-    void getMemberOrdersException() throws Exception {
-        when(orderMapper.getMemberOrders("username", null, PageRequest.of(1, 10))).thenReturn(new ArrayList<>());
-
-        String token = jwtTokenProvider.createToken("user");
-
-        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
-        params.add("name", "username");
-        mockMvc.perform(get("/api/v1/order/member-orders")
-                .header("Authorization", "Bearer " + token)
-                .params(params))
-                .andExpect(status().isInternalServerError());
-
-        verify(orderMapper, times(1)).getMemberOrders("username", null, PageRequest.of(1, 10));
-    }
-
-    @Test
-    @DisplayName("URI: /api/v1/order/member-orders : Unauthorized")
-    void getMembersUnauthorized() throws Exception {
-        mockMvc.perform(get("/api/v1/order/member-orders"))
-                .andExpect(status().isUnauthorized());
+    public void memberOrders() throws Exception {
+        mockMvc.perform(post("/api/v1/order/member-orders")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print());
     }
 }
